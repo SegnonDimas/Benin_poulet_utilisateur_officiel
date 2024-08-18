d97e7c2364d1f17c1bb4dc80d33cab2fd7ed0a82
SegnonDimas <segnondimas@gmail.com>
2024-08-18 12:41:55 +0100

    Separation des 'routes' du fichier main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,4 @@
-import 'package:benin_poulet/views/pages/connexion_pages/loginPage.dart';
-import 'package:benin_poulet/views/pages/connexion_pages/loginWithEmailPage.dart';
-import 'package:benin_poulet/views/pages/defaultRoutePage.dart';
-import 'package:benin_poulet/views/pages/inscription_pages/inscriptionPage.dart';
-import 'package:benin_poulet/views/pages/inscription_pages/inscription_vendeurPage.dart';
-import 'package:benin_poulet/views/pages/inscription_pages/signupWithEmailPage.dart';
-import 'package:benin_poulet/views/pages/started_pages/firstPage.dart';
-import 'package:benin_poulet/views/pages/started_pages/presentationPage.dart';
-import 'package:benin_poulet/views/pages/vendeur_pages/authentification/validationPage.dart';
-import 'package:benin_poulet/views/pages/vendeur_pages/v_commandeListPage.dart';
-import 'package:benin_poulet/views/pages/vendeur_pages/v_mainPage.dart';
-import 'package:benin_poulet/views/pages/vendeur_pages/v_presentationBoutiquePage.dart';
+import 'package:benin_poulet/routes.dart';
 import 'package:benin_poulet/views/themes/dark_mode.dart';
 import 'package:benin_poulet/views/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
@@ -89,22 +78,8 @@ class _MyAppState extends State<MyApp> {
       title: 'Bénin Poulet',
 
       //home: const LoginPage(),
-      routes: {
-        '/firstPage': (context) => const FirstPage(),
-        '/loginPage': (context) => const LoginPage(),
-        '/presentationPage': (context) => const PresentationPage(),
-        '/inscriptionPage': (context) => const InscriptionPage(),
-        '/loginWithEmailPage': (context) => const LoginWithEmailPage(),
-        '/signupWithEmailPage': (context) => const SignupWithEmailPage(),
-        '/inscriptionVendeurPage': (context) => const InscriptionVendeurPage(),
-        '/validationPage': (context) => const ValidationPage(),
-        '/vendeurMainPage': (context) => const VMainPage(),
-        '/vendeurPresentationBoutiquePage': (context) =>
-            const VPresentationBoutiquePage(),
-        '/vendeurCommandeListPage': (context) => const VCommandeListPage(),
-        '/defaultRoutePage': (context) => const DefaultRoutePage(),
-      },
-      //
+      routes: routes,
+
       initialRoute: '/firstPage',
     );
   }
---
+++ lib/routes.dart
@@ -0,0 +1,29 @@
+import 'package:benin_poulet/views/pages/connexion_pages/loginPage.dart';
+import 'package:benin_poulet/views/pages/connexion_pages/loginWithEmailPage.dart';
+import 'package:benin_poulet/views/pages/defaultRoutePage.dart';
+import 'package:benin_poulet/views/pages/inscription_pages/inscriptionPage.dart';
+import 'package:benin_poulet/views/pages/inscription_pages/inscription_vendeurPage.dart';
+import 'package:benin_poulet/views/pages/inscription_pages/signupWithEmailPage.dart';
+import 'package:benin_poulet/views/pages/started_pages/firstPage.dart';
+import 'package:benin_poulet/views/pages/started_pages/presentationPage.dart';
+import 'package:benin_poulet/views/pages/vendeur_pages/authentification/validationPage.dart';
+import 'package:benin_poulet/views/pages/vendeur_pages/v_commandeListPage.dart';
+import 'package:benin_poulet/views/pages/vendeur_pages/v_mainPage.dart';
+import 'package:benin_poulet/views/pages/vendeur_pages/v_presentationBoutiquePage.dart';
+import 'package:flutter/cupertino.dart';
+
+Map<String, Widget Function(BuildContext)> routes = {
+  '/firstPage': (context) => const FirstPage(),
+  '/loginPage': (context) => const LoginPage(),
+  '/presentationPage': (context) => const PresentationPage(),
+  '/inscriptionPage': (context) => const InscriptionPage(),
+  '/loginWithEmailPage': (context) => const LoginWithEmailPage(),
+  '/signupWithEmailPage': (context) => const SignupWithEmailPage(),
+  '/inscriptionVendeurPage': (context) => const InscriptionVendeurPage(),
+  '/validationPage': (context) => const ValidationPage(),
+  '/vendeurMainPage': (context) => const VMainPage(),
+  '/vendeurPresentationBoutiquePage': (context) =>
+      const VPresentationBoutiquePage(),
+  '/vendeurCommandeListPage': (context) => const VCommandeListPage(),
+  '/defaultRoutePage': (context) => const DefaultRoutePage(),
+};
